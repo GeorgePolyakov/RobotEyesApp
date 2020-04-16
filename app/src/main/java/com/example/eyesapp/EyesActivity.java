0995b101c84d6a0a3e4e4a936c755f1653174787
GeorgePolyakov <59865876+GeorgePolyakov@users.noreply.github.com>
2020-04-16 09:28:27 +0300

    third commit

--- app/src/main/java/com/example/eyesapp/EyesActivity.java
+++ app/src/main/java/com/example/eyesapp/EyesActivity.java
@@ -9,6 +9,6 @@ public class EyesActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_eyes);
+        setContentView(new EyesAnimation(this));
     }
 }
